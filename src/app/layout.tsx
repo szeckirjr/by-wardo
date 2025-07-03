9f081697f4c2c485127efb465189a974cac14dfc
Eduardo Szeckir <eszeckirjr@gmail.com>
2025-07-03 14:02:23 -0700

    fix twitter card and img size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Words",
+  title: "Words by Wardo",
   description: "A collection of words I find interesting.",
   authors: {
     url: "https://wardo.dev/",
@@ -20,23 +20,23 @@ export const metadata: Metadata = {
     icon: "/favicon_256x256.png",
   },
   openGraph: {
-    title: "Words",
+    title: "Words by Wardo",
     description: "A collection of words I find interesting.",
     url: "https://words.wardo.dev",
     siteName: "Words by Wardo",
     images: [
       {
         url: "https://words.wardo.dev/favicon_256x256.png",
-        width: 256,
-        height: 256,
+        width: 200,
+        height: 200,
       },
     ],
     locale: "en_US",
     type: "website",
   },
   twitter: {
-    card: "summary_large_image",
-    title: "Words",
+    card: "summary",
+    title: "Words by Wardo",
     description: "A collection of words I find interesting.",
     images: ["https://words.wardo.dev/favicon_256x256.png"],
     creator: "@szeckirjr",
